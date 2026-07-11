1ad8cb104dec0b7b8fb903fc54ee7ae35911fee8
nirantchavda <117748842+orpheusdark@users.noreply.github.com>
2026-07-11 11:21:47 +0530

    Add Coding Ninjas solution: Check Prime

---
+++ CodingNinjas/Check Prime.java
@@ -0,0 +1,27 @@
+public class Solution {
+ 
+    public static String isPrime(int num) {
+ 
+        //Your code goes here
+ 
+        if(num<=1){
+ 
+            return "NO";
+ 
+        }
+ 
+        for(int i=2;i<=Math.sqrt(num);i++){
+ 
+            if(num%i == 0){
+ 
+                return "NO";
+ 
+            }
+ 
+        }
+ 
+        return "YES";
+ 
+    }
+ 
+}
