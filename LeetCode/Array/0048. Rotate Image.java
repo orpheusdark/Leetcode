9beb49b6fdd21b98df5e227647994cf82f368758
nirantchavda <117748842+orpheusdark@users.noreply.github.com>
2026-07-20 08:22:42 +0530

    Solved Rotate Image

---
+++ LeetCode/Array/0048. Rotate Image.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length, k = n - 1;
+        for (int i = 0; i < n >> 1; i++)
+            for (int j = i; j < k - i; j++) {
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[k - j][i];
+                matrix[k - j][i] = matrix[k - i][k - j];
+                matrix[k - i][k - j] = matrix[j][k - i];
+                matrix[j][k - i] = t;
+            }
+    }
+}
