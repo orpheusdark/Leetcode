b9310fee2488a6a7debad814445f500e79d24dc4
nirantchavda <117748842+orpheusdark@users.noreply.github.com>
2026-07-20 09:44:15 +0530

    Add GFG solution: Make Zeroes

---
+++ GeeksForGeeks/Uncategorized/Make Zeroes.java
@@ -0,0 +1,36 @@
+}
+        }
+        
+        for (int i = 0; i<n; i++) {
+            for (int j = 0; j<m; j++)
+                {
+                    if (mat2[i][j]==0 && mat[i][j] == 0) {
+                    
+                    int sum = 0;
+                    if (i>0) {
+                        sum += mat2[i - 1][j];
+                        
+                        mat[i - 1][j] = 0;
+                    }
+                    if (i<n - 1) {
+                        sum += mat2[i + 1][j];
+                        
+                        mat[i + 1][j] = 0;
+                    }
+                    if (j>0) {
+                        sum += mat2[i][j - 1];
+                        
+                        mat[i][j - 1] = 0;
+                    }
+                    if (j<m - 1) {
+                        sum += mat2[i][j + 1];
+                        
+                        mat[i][j + 1] = 0;
+                    }
+                    mat[i][j] = sum;
+                }
+                }
+            
+        }
+    }
+}
