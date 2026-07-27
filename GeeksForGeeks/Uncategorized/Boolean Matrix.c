15b11cdf77d4e79292d7cd0c77790efe555edebf
nirantchavda <117748842+orpheusdark@users.noreply.github.com>
2026-07-27 09:18:55 +0530

    Add GFG solution: Boolean Matrix

---
+++ GeeksForGeeks/Uncategorized/Boolean Matrix.c
@@ -0,0 +1,24 @@
+class Solution {
+    void booleanMatrix(int mat[][]) {
+        // code here
+        int n = mat.length;
+        int m = mat[0].length;
+        boolean [] r = new boolean [n];
+        boolean [] c = new boolean [m];
+        for(int i = 0; i<n;i++){
+            for(int j = 0; j<m; j++){
+                if (mat[i][j] == 1){
+                    r[i] = true;
+                    c[j] = true;
+                }
+            }
+        }
+        for(int i = 0; i<n;i++){
+            for(int j = 0; j<m; j++){
+                if(r[i] || c[j]){
+                    mat[i][j] = 1;
+                } 
+            }
+        }
+     }
+}
